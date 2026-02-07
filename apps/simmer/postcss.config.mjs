fb2cd3786d0d9e0995cbad8fac985b4b7df103fc
thebigthing313 <adriankabigting@gmail.com>
2026-02-07 18:42:51 -0500

    feat: hello world

---
+++ apps/simmer/postcss.config.mjs
@@ -0,0 +1,1 @@
+export { default } from '@simmer/ui/postcss.config';
---
+++ apps/simmer/vite.config.ts
@@ -0,0 +1,15 @@
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [
+		tsConfigPaths(),
+		tanstackRouter({
+			autoCodeSplitting: true,
+			target: 'react',
+		}),
+		viteReact(),
+	],
+});
