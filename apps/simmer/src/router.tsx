df48e70f6b1b4930a7fa232c3655deee5def535a
thebigthing313 <adriankabigting@gmail.com>
2026-02-11 14:00:50 -0500

    feat: add router context to enable dynamic routing capabilities

--- apps/simmer/src/router.tsx
+++ apps/simmer/src/router.tsx
@@ -1,8 +1,11 @@
 import { createRouter } from '@tanstack/react-router';
+import type { MyRouterContext } from './routes/__root';
 import { routeTree } from './routeTree.gen';
 
+const routeContext: MyRouterContext = { mainOutlet: null };
 export function getRouter() {
 	const router = createRouter({
+		context: routeContext,
 		routeTree,
 		scrollRestoration: true,
 	});
