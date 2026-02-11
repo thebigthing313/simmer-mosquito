448f69f5167cad7813a81f2aedddcc3e489671d9
thebigthing313 <adriankabigting@gmail.com>
2026-02-11 14:00:54 -0500

    feat: add create trap route with main outlet header and description

---
+++ apps/simmer/src/routes/(app)/adult-surveillance/create-trap.tsx
@@ -0,0 +1,17 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/(app)/adult-surveillance/create-trap')({
+	beforeLoad: () => {
+		return {
+			mainOutlet: {
+				header: 'Create Trap',
+				description: 'Add a new surveillance trap',
+			},
+		};
+	},
+	component: RouteComponent,
+});
+
+function RouteComponent() {
+	return <div>Hello "/(app)/adult-surveillance/create-trap"!</div>;
+}
