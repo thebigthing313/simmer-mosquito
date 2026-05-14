6e86532b42f71bed7b2f2137e3d63b1fbdb243ed
thebigthing313 <adriankabigting@gmail.com>
2026-05-14 14:23:02 -0400

    Remove worker domain sample dependency

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,21 +1,14 @@
 import { setInterval } from 'node:timers';
 import { readEnv } from '@simmer-mosquito/config';
-import { calculateTrapNightRate } from '@simmer-mosquito/domain';
 
 const env = readEnv();
 const intervalMs = 60_000;
 
 async function runSurveillanceCycle(): Promise<void> {
-	const sampleRate = calculateTrapNightRate({
-		mosquitoCount: 42,
-		trapNights: 7,
-	});
-
 	console.log(
 		JSON.stringify({
-			event: 'surveillance_cycle_completed',
+			event: 'worker_heartbeat',
 			environment: env.nodeEnv,
-			sampleRate,
 		}),
 	);
 }
